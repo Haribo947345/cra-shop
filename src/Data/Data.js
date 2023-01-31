d1341f4980196876e05b6935ade086ae7fcee48b
junny1995 <while0818@gmail.com>
2023-01-31 18:11:43 +0900

    feat: 아이폰,아이패드 더미데이터 추가

--- src/Data/Data.js
+++ src/Data/Data.js
@@ -1,9 +1,22 @@
+// iPhone
 import iPhone14ProMax from "../Img/iPhone14ProMax.png";
 import iPhone14Pro from "../Img/iPhone14Pro.png";
+import iPhone14 from "../Img/iPhone14.png";
+import iPhone13 from "../Img/iPhone13.png";
+import iPhoneSE from "../Img/iPhoneSE.png";
+
+// iPad
 import iPad12IMG from "../Img/iPad12.9.png";
-import IPadAir from "../Img/iPadAir.png";
+import iPadAir from "../Img/iPadAir.png";
+import iPad10thgen from "../Img/iPad10thgen.png";
+import iPad9thgen from "../Img/iPad9thgen.png";
+import iPadMini from "../Img/iPadMini6thgen.png";
+
+// MacBook
 import MacBookAir from "../Img/MacBookAir.png";
 import MacBookPro from "../Img/MacBookPro.png";
+
+// Acc
 import AirpodsMax from "../Img/AirpodsMax.png";
 import AirpodsPro from "../Img/AirpodsPro.png";
 
@@ -24,41 +37,83 @@ export const MainHomeData = [
   },
   {
     ID: 3,
-    CATEGORY: "iPad",
-    NAME: "iPad Air",
-    PRICE: "929,000",
-    IMG: IPadAir,
+    CATEGORY: "iPhone",
+    NAME: "iPhone 14",
+    PRICE: "1,250,000",
+    IMG: iPhone14,
   },
   {
     ID: 4,
+    CATEGORY: "iPhone",
+    NAME: "iPhone 13",
+    PRICE: "950,000",
+    IMG: iPhone13,
+  },
+  {
+    ID: 5,
+    CATEGORY: "iPhone",
+    NAME: "iPhone SE",
+    PRICE: "650,000",
+    IMG: iPhoneSE,
+  },
+  {
+    ID: 6,
+    CATEGORY: "iPad",
+    NAME: "iPad Air",
+    PRICE: "929,000",
+    IMG: iPadAir,
+  },
+  {
+    ID: 7,
     CATEGORY: "iPad",
     NAME: "iPad Pro",
     PRICE: "1,729,000",
     IMG: iPad12IMG,
   },
   {
-    ID: 5,
+    ID: 8,
+    CATEGORY: "iPad",
+    NAME: "iPad 10th gen",
+    PRICE: "679,000",
+    IMG: iPad10thgen,
+  },
+  {
+    ID: 9,
+    CATEGORY: "iPad",
+    NAME: "iPad 9th gen",
+    PRICE: "499,000",
+    IMG: iPad9thgen,
+  },
+  {
+    ID: 10,
+    CATEGORY: "iPad",
+    NAME: "iPad Pro",
+    PRICE: "769,000",
+    IMG: iPadMini,
+  },
+  {
+    ID: 11,
     CATEGORY: "MacBook",
     NAME: "MacBook Air with M2 chip",
     PRICE: "1,690,000",
     IMG: MacBookAir,
   },
   {
-    ID: 6,
+    ID: 12,
     CATEGORY: "MacBook",
     NAME: "MacBook Pro with M1 chip",
     PRICE: "3,360,000",
     IMG: MacBookPro,
   },
   {
-    ID: 7,
+    ID: 13,
     CATEGORY: "Acc",
     NAME: "Airpods Max",
     PRICE: "769,000",
     IMG: AirpodsMax,
   },
   {
-    ID: 8,
+    ID: 14,
     CATEGORY: "Acc",
     NAME: "Airpods Pro",
     PRICE: "359,000",
@@ -81,35 +136,77 @@ export const MainiPhoneData = [
     PRICE: "1,750,000",
     IMG: iPhone14ProMax,
   },
+  {
+    ID: 3,
+    CATEGORY: "iPhone",
+    NAME: "iPhone 14",
+    PRICE: "1,250,000",
+    IMG: iPhone14,
+  },
+  {
+    ID: 4,
+    CATEGORY: "iPhone",
+    NAME: "iPhone 13",
+    PRICE: "950,000",
+    IMG: iPhone13,
+  },
+  {
+    ID: 5,
+    CATEGORY: "iPhone",
+    NAME: "iPhone SE",
+    PRICE: "650,000",
+    IMG: iPhoneSE,
+  },
 ];
 
 export const MainiPadData = [
   {
-    ID: 3,
+    ID: 6,
     CATEGORY: "iPad",
     NAME: "iPad Air",
     PRICE: "929,000",
-    IMG: IPadAir,
+    IMG: iPadAir,
   },
   {
-    ID: 4,
+    ID: 7,
     CATEGORY: "iPad",
     NAME: "iPad Pro",
     PRICE: "1,729,000",
     IMG: iPad12IMG,
   },
+  {
+    ID: 8,
+    CATEGORY: "iPad",
+    NAME: "iPad 10th gen",
+    PRICE: "679,000",
+    IMG: iPad10thgen,
+  },
+  {
+    ID: 9,
+    CATEGORY: "iPad",
+    NAME: "iPad 9th gen",
+    PRICE: "499,000",
+    IMG: iPad9thgen,
+  },
+  {
+    ID: 10,
+    CATEGORY: "iPad",
+    NAME: "iPad Pro",
+    PRICE: "769,000",
+    IMG: iPadMini,
+  },
 ];
 
 export const MainMacBookData = [
   {
-    ID: 5,
+    ID: 11,
     CATEGORY: "MacBook",
     NAME: "MacBook Air with M2 chip",
     PRICE: "1,690,000",
     IMG: MacBookAir,
   },
   {
-    ID: 6,
+    ID: 12,
     CATEGORY: "MacBook",
     NAME: "MacBook Pro with M1 chip",
     PRICE: "3,360,000",
@@ -119,14 +216,14 @@ export const MainMacBookData = [
 
 export const MainAccData = [
   {
-    ID: 7,
+    ID: 13,
     CATEGORY: "Acc",
     NAME: "Airpods Max",
     PRICE: "769,000",
     IMG: AirpodsMax,
   },
   {
-    ID: 8,
+    ID: 14,
     CATEGORY: "Acc",
     NAME: "Airpods Pro",
     PRICE: "359,000",
