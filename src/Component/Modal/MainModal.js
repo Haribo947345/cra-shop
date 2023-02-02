c404c9839c14a8d48e91d52f8c4f7f70cc83597e
junny1995 <while0818@gmail.com>
2023-02-02 20:45:26 +0900

    비로그인 접근페이지 모달추가

---
+++ src/Component/Modal/MainModal.js
@@ -0,0 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import "tw-elements";
+
+function MainModal(props) {
+  const { title, body, close } = props;
+  const navigate = useNavigate();
+  return (
+    <div
+      className="modal fade fixed top-0 hidden left-0 w-full h-full outline-none overflow-x-hidden overflow-y-auto"
+      id="exampleModalCenter"
+      tabIndex="-1"
+      aria-labelledby="exampleModalCenterTitle"
+      aria-modal="true"
+      role="dialog"
+    >
+      <div className="modal-dialog modal-dialog-centered relative w-auto pointer-events-none">
+        <div className="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-slate-800 bg-clip-padding rounded-md outline-none text-current">
+          <div className="modal-header flex flex-shrink-0 items-center justify-between p-4 rounded-t-md">
+            <h5
+              className="text-xl font-medium leading-normal text-white"
+              id="exampleModalScrollableLabel"
+            >
+              {title}
+            </h5>
+          </div>
+          <div className="modal-body relative p-4 text-white">
+            <p>{body}</p>
+          </div>
+          <div className="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 rounded-b-md">
+            <button
+              type="button"
+              className="inline-block px-6 py-2.5 bg-slate-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-slate-700 hover:shadow-lg transition duration-150 ease-in-out"
+              data-bs-dismiss="modal"
+            >
+              {close}
+            </button>
+            <button
+              type="button"
+              data-bs-dismiss="modal"
+              onClick={() => navigate("/login")}
+              className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg transition duration-150 ease-in-out ml-1"
+            >
+              로그인하기
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainModal;
--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -1,14 +1,29 @@
+import { useNavigate } from "react-router-dom";
+import MainModal from "../Component/Modal/MainModal";
+
 function ErrorPage() {
+  const navigate = useNavigate();
+  const close2 = () => {
+    navigate("/login");
+  };
   return (
-    <section className="flex justify-center items-center last:text-gray-400 bg-gray-900 body-font min-h-full pb-24">
-      <div className="container px-5 py-24 mx-auto">
-        <h1 className="flex justify-center text-2xl text-white mb-7">
-          잘못된 방식으로 접근하셨습니다!
-          <br />
-          로그인이 필요해요!
-        </h1>
+    <div className="flex min-h-full">
+      <button
+        className="flex m-auto p-7 bg-orange-800 text-white font-medium text-lg uppercase rounded shadow-md hover:bg-red-900 hover:shadow-lg transition duration-150 ease-in-out"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModalCenter"
+      >
+        접근할 수 없습니다!
+      </button>
+      <div>
+        <MainModal
+          title="접근 오류 알림"
+          body="비정상적인 경로로 접근하셨습니다!"
+          close="닫기"
+          close2={close2}
+        />
       </div>
-    </section>
+    </div>
   );
 }
 
